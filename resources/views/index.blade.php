5e6f15fdf2a272aaaaf55b8ec3b29e8ab147c4ce
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 15:55:10 +0900

    [Add] ログインの確認

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,12 @@ height: 30px;
 @section('title', 'index.blade.php')
 
 @section('content')
+@if (Auth::check())
+<p>ログイン中ユーザー: {{$user->name . ' メール' . $user->email . ''}}</p>
+@else
+<p>ログインしてください。（<a href="/login">ログイン</a> |
+<a href="/register">登録</a>）</p>
+@endif
 <table>
 <tr>
 <th>Data</th>
