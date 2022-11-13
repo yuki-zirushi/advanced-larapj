008eeefff5781173050928e7b0f7a89c6574a391
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 13:16:23 +0900

    [Add] relation belongsTo

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,6 +11,11 @@ class Book extends Model
 
     public function getTitle()
     {
-        return 'ID' . $this->id . ':' . $this->title;
+        return 'ID'.$this->id . ':' . $this->title . ' 著者:' . optional($this->author)->name;
+    }
+
+    public function author()
+    {
+        return $this->belongsTo('App\Models\Author');
     }
 }
