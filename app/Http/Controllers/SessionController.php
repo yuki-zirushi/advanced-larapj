21f6419df5d7eb6da7cba81e6105374babbae7af
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 14:19:56 +0900

    [Add] method getSes postSes

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    //
+    public function getSes(Request $request)
+    {
+        $data = $request->session()->get('txt');
+        return view('session', ['data'=>$data]);
+    }
+
+    public function postSes(Request $request)
+    {
+        $txt = $request->input;
+        $request->session()->put('txt', $txt);
+        return redirect('/session');
+    }
 }
