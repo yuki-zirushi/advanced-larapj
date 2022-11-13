22842aa4b3d1ea487e85f5728480914318fe96aa
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 13:06:43 +0900

    [Add] route relate

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::prefix('book')->group(function () {
     Route::get('/add', [BookController::class, 'add']);
     Route::post('/add', [BookController::class, 'create']);
 });
+
+Route::get('/relation', [AuthorController::class, 'relate']);
