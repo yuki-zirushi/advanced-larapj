ffdb862d1928401af968407c5a2af79100de170c
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 16:55:12 +0900

    [Update] シーダーへのファクトリ設定

--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Author;
 
 class AuthorsTableSeeder extends Seeder
@@ -14,29 +15,6 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'name' => 'tony',
-            'age' => 35,
-            'nationality' => 'American'
-        ];
-        Author::create($param);
-        $param = [
-            'name' => 'jack',
-            'age' => 20,
-            'nationality' => 'British'
-        ];
-        Author::create($param);
-        $param = [
-            'name' => 'sara',
-            'age' => 45,
-            'nationality' => 'Egyptian'
-        ];
-        Author::create($param);
-        $param = [
-            'name' => 'saly',
-            'age' => 31,
-            'nationality' => 'Chinese'
-        ];
-        Author::create($param);
+        Author::factory()->count(3)->create();
     }
 }
