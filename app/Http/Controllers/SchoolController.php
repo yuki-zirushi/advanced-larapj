a045dd5ae3fbc643ac16bd4f5d5f6b69fa9c2645
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 19:49:10 +0900

    [Update] SchoolController

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\School;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class SchoolController extends Controller
 {
-    //
+    public function fillSchool() {
+        $school = new School();
+        $uuid = (string)Str::uuid();
+        $school->fill([
+            'uuid' =>  $uuid,
+            'name' => 'COACHTECH',
+            ]) ;
+        $school->save();
+    }
+    public function createSchool() {
+        $uuid = (string)Str::uuid();
+        School::create([
+            'uuid' =>  $uuid,
+            'name' => 'COACHTECH-Pro',
+        ]);
+    }
+    public function insertSchool() {
+        $school = new School();
+        $uuid = (string)Str::uuid();
+        $school::insert([
+            'uuid' =>  $uuid,
+            'name' => 'COACHTECH-Lab',
+        ]);
+    }
 }
