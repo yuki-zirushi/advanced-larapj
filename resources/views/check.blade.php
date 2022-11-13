5d1f129107b58431ef4103a0a8be21e9cadab3a6
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 14:35:12 +0900

    [Create] view 確認画面

---
+++ resources/views/check.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+<style>
+p {
+  background-color: #289ADC;
+  color: white;
+  padding: 5px 10px;
+  width: 500px;
+}
+</style>
+@section('title', 'check.blade.php')
+
+@section('content')
+<form action="/back" method="GET">
+  @csrf
+  <p>入力した値は「{{ $data }}」でお間違いないでしょうか</p>
+  <button type="submit" class="check-btn" value="true">入力画面に戻る</button>
+<form>
+@endsection
