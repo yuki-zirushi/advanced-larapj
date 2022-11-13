c5fdd0c54b3f9c0fb013a3b96ac2c235d451c108
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 10:19:09 +0900

    [Update] データの取得

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
 }
