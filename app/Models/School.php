6b836901012e3066cd1be8f05acbf40e0893ad44
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 19:44:00 +0900

    [Update] model School

--- app/Models/School.php
+++ app/Models/School.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $fillable = [
+        'uuid', 'name'
+    ];
 }
