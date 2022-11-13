370bdf2b7dba1ab08981440603560e9b763d8c88
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 16:42:21 +0900

    [Edit] HelloTest.php データベースのテスト

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\User;
+use App\Models\User;
 
 class HelloTest extends TestCase
 {
@@ -14,14 +14,20 @@ class HelloTest extends TestCase
      *
      * @return void
      */
+
+    use RefreshDatabase;
+
     public function testHello()
     {
-        $this->assertTrue(true);
-
-        $response = $this->get('/');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        User::factory()->create([
+            'name'=>'aaa',
+            'email'=>'bbb@ccc.com',
+            'password'=>'test12345'
+        ]);
+        $this->assertDatabaseHas('users',[
+            'name'=>'aaa',
+            'email'=>'bbb@ccc.com',
+            'password'=>'test12345'
+        ]);
     }
 }
