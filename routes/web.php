5118b53609d6cf643cdcaabf634a545ec9a916b9
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 15:57:58 +0900

    [Fix] route

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\SessionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/home', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+Route::get('/author/{author}',[AuthorController::class, 'bind']);
+Route::prefix('book')->group(function () {
+Route::get('/', [BookController::class, 'index']);
+Route::get('/add', [BookController::class, 'add']);
+Route::post('/add', [BookController::class, 'create']);
+});
+Route::get('/relation', [AuthorController::class, 'relate']);
+
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
 
 Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
