9cccc9adebac28fe714397e2261b9e533138314c
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 16:03:58 +0900

    [Add] action check checkUser

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -102,4 +102,23 @@ class AuthorController extends Controller
         $param = ['hasbooks' => $hasbooks, 'nobooks' => $nobooks];
         return view('author.index',$param);
     }
+
+    public function check(Request $request)
+    {
+        $text = ['text' => 'ログインして下さい。'];
+        return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+            $text =   Auth::user()->name . 'さんがログインしました';
+        } else {
+            $text = 'ログインに失敗しました';
+        }
+        return view('auth', ['text' => $text]);
+    }
 }
