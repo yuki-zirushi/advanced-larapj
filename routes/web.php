ce50631bbd91b8c18541a66a227c2be03ae10661
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 16:06:41 +0900

    [Add] route check checkUser

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/auth', [AuthorController::class, 'check']);
+Route::post('/auth', [AuthorController::class, 'checkUser']);
