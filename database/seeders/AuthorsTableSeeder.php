7a3f0dddb3b543754098cd7cb6bbb5b92fd9cbbd
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 11:31:36 +0900

    [Update] seeder

--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Author;
 
 class AuthorsTableSeeder extends Seeder
 {
@@ -13,6 +14,29 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'name' => 'tony',
+            'age' => 35,
+            'nationality' => 'American'
+        ];
+        Author::create($param);
+        $param = [
+            'name' => 'jack',
+            'age' => 20,
+            'nationality' => 'British'
+        ];
+        Author::create($param);
+        $param = [
+            'name' => 'sara',
+            'age' => 45,
+            'nationality' => 'Egyptian'
+        ];
+        Author::create($param);
+        $param = [
+            'name' => 'saly',
+            'age' => 31,
+            'nationality' => 'Chinese'
+        ];
+        Author::create($param);
     }
 }
