96f041014d1de4600ead8f34adace2dd4c7f3db0
yuki-zirushi <yuko.12.151@gmail.com>
2022-11-13 14:25:00 +0900

    [Add] route getSes postSes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Middleware\FirstMiddleware;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,3 +37,5 @@ Route::prefix('book')->group(function () {
 });
 
 Route::get('/relation', [AuthorController::class, 'relate']);
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
